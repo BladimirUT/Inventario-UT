ec8795b23cc76bce6fdfcdd8e06ec0637f649081
BladimirUT <105954519+BladimirUT@users.noreply.github.com>
2022-07-12 10:27:49 -0500

    Add files via upload

---
+++ layouts/admin_menu.php
@@ -0,0 +1,52 @@
+<ul>
+  <li>
+    <a href="admin.php">
+      <i class="glyphicon glyphicon-home"></i>
+      <span>Menu</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-user"></i>
+      <span>Accesos</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="group.php">Administrador</a> </li>
+      <li><a href="users.php">Agregar Usuarios</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="categorie.php" >
+      <i class="glyphicon glyphicon-indent-left"></i>
+      <span>Categorías</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-large"></i>
+      <span>Productos</span>
+    </a>
+    <ul class="nav submenu">
+       <li><a href="product.php">Administrar productos</a> </li>
+       <li><a href="add_product.php">Agregar productos</a> </li>
+   </ul>
+  </li>
+  <li>
+    
+  </li>
+  <li>
+  
+      <ul class="nav submenu">
+         <li><a href="sales.php">Administrar ventas</a> </li>
+         <li><a href="add_sale.php">Agregar venta</a> </li>
+     </ul>
+  </li>
+  <li>
+    
+      <ul class="nav submenu">
+        <li><a href="sales_report.php">Ventas por fecha </a></li>
+        <li><a href="monthly_sales.php">Ventas mensuales</a></li>
+        <li><a href="daily_sales.php">Ventas diarias</a> </li>
+      </ul>
+  </li>
+</ul>
